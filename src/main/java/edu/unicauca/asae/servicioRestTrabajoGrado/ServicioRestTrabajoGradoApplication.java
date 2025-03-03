ec894174c80a247331468d4682957cc54152b5a1
Jaycom17 <juanorejuela499@gmail.com>
2025-03-03 15:24:45 -0500

    first commir

---
+++ src/main/java/edu/unicauca/asae/servicioRestTrabajoGrado/ServicioRestTrabajoGradoApplication.java
@@ -0,0 +1,13 @@
+package edu.unicauca.asae.servicioRestTrabajoGrado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicioRestTrabajoGradoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicioRestTrabajoGradoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/unicauca/asae/servicioRestTrabajoGrado/ServicioRestTrabajoGradoApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.unicauca.asae.servicioRestTrabajoGrado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicioRestTrabajoGradoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
